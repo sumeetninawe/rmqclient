112a7a57af862c3e7ed776ad2fd5c0815fb9daf9
Sumeet <sumeet.ninawe@gmail.com>
2019-04-15 16:20:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var amqp = require('amqplib/callback_api');
+var os = require('os');
+var channel;
+var q = 'events';
+amqp.connect('amqp://admin:admin@sumeet.life:5672/', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    channel = ch;
+    ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.sendToQueue(q, Buffer.from('CONN'));
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+  });
+});
+
+var eventHandler = (code) => {
+    console.log(`About to exit with code: ${code}`);
+    var eveObject = {};
+    eveObject.scode = code;
+    eveObject.cCode = 'EXIT';
+    eveObject.host = os.hostname();
+    eveObject.type = 'receiver';
+    channel.sendToQueue(q, Buffer.from(JSON.stringify(eveObject)));
+    console.log(" [x] Sent %s", JSON.stringify(eveObject));
+}
+
+//Exit Events
+process.on('SIGINT', eventHandler);
+process.on('SIGTERM', eventHandler);
+process.on('SIGBREAK', eventHandler);
+/*process.on('SIGKILL', eventHandler);
+process.on('SIGSTOP', eventHandler);*/
