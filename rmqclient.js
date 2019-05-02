8302b41a19ea047138f532a3b813210d008716c1
Sumeet <sumeet.ninawe@gmail.com>
2019-05-03 02:07:41 +0530

    ok

--- rmqclient.js
+++ rmqclient.js
@@ -1,13 +1,20 @@
 var amqp = require('amqplib/callback_api');
 var os = require('os');
+var ipc = require('node-ipc');
 var channel;
 var q = 'events';
+
+
+
 amqp.connect('amqp://admin:admin@sumeet.life:5672/', function(err, conn) {
   conn.createChannel(function(err, ch) {
     channel = ch;
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.sendToQueue(q, Buffer.from('CONN'));
+    for(var i = 0; i < 5; i++){
+      eventHandler('CONN');
+    }    
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
     }, {noAck: true});
@@ -15,7 +22,7 @@ amqp.connect('amqp://admin:admin@sumeet.life:5672/', function(err, conn) {
 });
 
 var eventHandler = (code) => {
-    console.log(`About to exit with code: ${code}`);
+    console.log(`About to handle with code: ${code}`);
     var eveObject = {};
     eveObject.scode = code;
     eveObject.cCode = 'EXIT';
@@ -33,3 +40,15 @@ process.on('EVE007', eventHandler);
 process.on('exit', eventHandler);
 /*process.on('SIGKILL', eventHandler);
 process.on('SIGSTOP', eventHandler);*/
+
+
+//event simulation logic
+ipc.config.id = 'rmq-server';
+ipc.config.retry = 1500;
+ipc.config.silent = true;
+ipc.serve(() => ipc.server.on('eve007', (message) => {
+  for(var i = 0; i < 5; i++){
+    eventHandler('EVE007');
+  } 
+}));
+ipc.server.start();
